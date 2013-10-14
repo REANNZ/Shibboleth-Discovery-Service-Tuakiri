REANNZ/Shibboleth-Discovery-Service-Tuakiri

commit 47ff0decdfda45673e13149405f119f512b68081

Config: Keep IdP siteSets in the config file order
Solves \#8

Modify the config file parsing in WayfService to preserve the order in
which federations (MetadataProvider stored as IdPSiteSet) are listed in
the configuration file.

This means switching from a Hashtable to a List - also in the arguments
passed to the DiscoveryServiceHandler constructor.  Inside the
constructor, construct on-the-fly a Hashtable for looking up IdPSiteSets
by their identifier but add them to the internal config for the handler
from the list, preserving their order.

Also in DSH.setupReturnAddress, when looking up the SP by spName, break
out of the loop after first match to successfully implement the logic
requested in \#8.

src/main/java/edu/internet2/middleware/shibboleth/wayf/DiscoveryServiceHandler.java
@@ -174,11 +174,18 @@ public class DiscoveryServiceHandler {
      * @throws ShibbolethConfigurationException - if we find something odd in the config file. 
      */
     protected DiscoveryServiceHandler(Element config, 
-                                      Hashtable <String, IdPSiteSet> federations,
+                                      List <IdPSiteSet> federations,
                                       Hashtable <String, Plugin> plugins, 
                                       HandlerConfig defaultConfig) throws ShibbolethConfigurationException
     {
         siteSets = new ArrayList <IdPSiteSet>(federations.size());
+
+        // create a map based on the configuration-ordered list of federations
+        Map <String,IdPSiteSet> federationsMap = new Hashtable<String, IdPSiteSet>();
+        for (IdPSiteSet federation: federations ) { 
+            federationsMap.put(federation.getIdentifier(), federation); 
+        };
+
         this.plugins = new ArrayList <Plugin>(plugins.size());
 
         //
@@ -216,7 +223,7 @@ public class DiscoveryServiceHandler {
                     
             attribute = ((Element) list.item(i)).getAttribute("identifier");
                     
-                IdPSiteSet siteset = federations.get(attribute);
+                IdPSiteSet siteset = federationsMap.get(attribute);
                 
                 if (siteset == null) {
                     LOG.error("Handler " + location + ": could not find metadata for <Federation> with identifier " + attribute + ".");
@@ -231,7 +238,7 @@ public class DiscoveryServiceHandler {
             //
             // No Federations explicitly named pick em all
             //
-            siteSets.addAll(federations.values());
+            siteSets.addAll(federations);
         }
         
         //
@@ -425,6 +432,7 @@ public class DiscoveryServiceHandler {
             //
             // Only do work if the SP makes sense
             //
+            LOG.debug("Checking federation "+metadataProvider.getIdentifier()+" for SP "+spName);
 
             if (metadataProvider.containsSP(spName)) {
                
@@ -456,6 +464,7 @@ public class DiscoveryServiceHandler {
                     }
                 }
             }
+            break;
         }
         if (!foundSPName) {
             LOG.error("Could not locate SP " + spName + " in metadata");

src/main/java/edu/internet2/middleware/shibboleth/wayf/WayfService.java
@@ -275,7 +275,7 @@ public class WayfService extends HttpServlet {
             //
             // Load metadata
             //
-            Hashtable <String, IdPSiteSet> siteSets = new Hashtable <String, IdPSiteSet>();
+            List <IdPSiteSet> siteSets = new ArrayList <IdPSiteSet>();
     
             itemElements = doc.getDocumentElement().getElementsByTagNameNS(XMLConstants.CONFIG_NS,
                             "MetadataProvider");
@@ -286,7 +286,7 @@ public class WayfService extends HttpServlet {
                 
                 IdPSiteSet siteset = new IdPSiteSet(element, parser, defaultHandlerConfig.getWarnOnBadBinding());
                 
-                siteSets.put(siteset.getIdentifier(), siteset);
+                siteSets.add(siteset);
             }
             if (siteSets.size() < 1) {
                 LOG.error("No Metadata Provider metadata loaded.");

commit 74e2e87c7bb68b34558521fe7fe79b67d1fe5b11

DSH: fix an error in SP lookup
Fix an error in DiscoveryServiceHandler introduced in 47ff0de

Fixes \#9

src/main/java/edu/internet2/middleware/shibboleth/wayf/DiscoveryServiceHandler.java
@@ -468,8 +468,8 @@ public class DiscoveryServiceHandler {
                         }
                     }
                 }
+                break;
             }
-            break;
         }
         if (!foundSPName) {
             LOG.error("Could not locate SP " + spName + " in metadata");